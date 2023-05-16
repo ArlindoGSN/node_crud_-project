72fb8a6c0c10b20f6cc48d3039aa38ecbca7c7c7
Arlindo Neto <103236672+ArlindoGSN@users.noreply.github.com>
2023-05-15 23:44:22 -0300

    modified file server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
-const app = require("./app");
-
-require("dotenv").config();
+const app = require('./app');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log("run in ${PORT}"));
+app.listen(PORT, () => console.log(`Server running or port ${PORT}`));
